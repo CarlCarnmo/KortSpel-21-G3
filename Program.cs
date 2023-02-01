7e67a984f7ea6e7c623f8f67e21f8537b46ca5ec
Carl Carnmo <Carl.Carnmo@gmail.com>
2023-02-01 21:55:10 +0100

    some code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace KortSpel_21_G3
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static KortSpel_21_G3.Program;
+using static KortSpel_21_G3._Deck;
+
+
+namespace KortSpel_21_G3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Deck deck = new Deck();
+            deck.Reset();
+            Console.WriteLine(deck.Cards.Count);
+
         }
     }
 }
