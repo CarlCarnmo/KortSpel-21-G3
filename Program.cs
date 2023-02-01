cfb38a046f0d152e215ba27e558208825d01d6a0
Carl Carnmo <Carl.Carnmo@gmail.com>
2023-02-01 12:26:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KortSpel_21_G3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
